f0588ccaecde2737a63d482dc63b21442ce58f0c
Kang-Min-Gi <mingi.kang@schoolsharing.co.kr>
2020-01-02 01:53:30 +0900

    내장 톰캣을 강제로 설정하는 방법과 리스너

--- src/main/java/kr/co/schoolsharing/inflearn/InflearnApplication.java
+++ src/main/java/kr/co/schoolsharing/inflearn/InflearnApplication.java
@@ -1,29 +1,49 @@
 package kr.co.schoolsharing.inflearn;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
-//@ComponentScan
-//@EnableAutoConfiguration
-//@SpringBootConfiguration
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@SpringBootApplication
 public class InflearnApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(InflearnApplication.class, args);
+    public static void main(String[] args) throws Exception{
+//        SpringApplication.run(InflearnApplication.class, args);
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(53340);
 
-        /* //
-        SpringApplication app = new SpringApplication(InflearnApplication.class);
-        app.setWebApplicationType(WebApplicationType.NONE);
-        app.run(args);
-         */
-
+        Context context = tomcat.addContext("/", "/");
+        HttpServlet httpServlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("<html><head><title>");
+                writer.println("HEY, TOMCAT");
+                writer.println("</title></head>");
+                writer.println("<body><h1>hello tomcat</h1><body>");
+                writer.println("</html>");
+            }
+        };
+        String servletName = "helloServlet";
+        tomcat.addServlet("/", servletName, httpServlet);
+        context.addServletMappingDecoded("/hello", servletName);
+        tomcat.start();
+//        tomcat.getServer().await();
     }
 
 }
