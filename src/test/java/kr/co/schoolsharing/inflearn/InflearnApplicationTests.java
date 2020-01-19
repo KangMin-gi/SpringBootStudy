0c60ef54dad948d71e26f6697cd8aed6da607a97
Kang-Min-Gi <mingi.kang@schoolsharing.co.kr>
2020-01-19 17:47:49 +0900

    MVC 마무리

--- src/test/java/kr/co/schoolsharing/inflearn/InflearnApplicationTests.java
+++ src/test/java/kr/co/schoolsharing/inflearn/InflearnApplicationTests.java
@@ -1,16 +1,21 @@
 package kr.co.schoolsharing.inflearn;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class InflearnApplicationTests {
 
-
-    @Test
-    void contextLoads() {
-    }
 
 }
